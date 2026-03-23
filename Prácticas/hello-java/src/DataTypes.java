229a03656e93df6fbc0f212728c3d5b61a1a7272
cybermito <cybermito@gmail.com>
2026-03-23 06:59:35 +0100

    Clase: Tipos de datos primitivos

---
+++ Prácticas/hello-java/src/DataTypes.java
@@ -0,0 +1,38 @@
+public class DataTypes {
+
+    static void main(String[] args) {
+        //Tipos de datos en Java (Primitivos)
+
+        //Enteros
+        int myInt = 10;
+        System.out.println(myInt);
+        byte myByte = 127;
+        System.out.println(myByte);
+        long myLong = 1234567890;
+        System.out.println(myLong);
+
+        //Decimales
+        float myFloat = 12.34352f; //Número máximo de dígitos decimales.
+        System.out.println(myFloat);
+        double myDouble = 3.1417383485609123; //Número máximo de dígitos decimales. (precisión decímal)
+        System.out.println(myDouble);
+
+        //Carácter
+        char myChar = 'a'; //Solo puede guardar un carácter y se ha de escribir con comillas simples.
+        System.out.println(myChar);
+
+        //Boolean
+        boolean myBoolean = true; //Valores true o false.
+        System.out.println(myBoolean);
+
+        //String no es un tipo de dato primitivo, pero se suele poner aquí ya que es el primero en usarse.
+        String  myString = "Hello World";
+        System.out.println(myString);
+
+
+
+
+
+
+    }
+}
